a5b68b480c4c95a25a918abf252366acdde1381e
Kleber Freire <ch4r4d4@gmail.com>
2018-04-20 15:55:52 -0300

    PDO DOA - SELECT, LIST, INSERT, UPDATE

---
+++ DAO/index.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once("config.php");
+
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+//echo json_encode($usuarios);
+
+//---carrega 1 usuario---
+//$root = new Usuario();
+//$root->loadById(2);
+//echo $root;
+
+//---carregar uma lista de usuarios---
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//---carregar uma lista  de usuarios buscando pelo $login---
+//$search = Usuario::search("k");
+//echo json_encode($search);
+
+//---carregar um usuario usando login e a senha---
+//$usuario = new Usuario();
+//$usuario->login("teste", "21345");
+//echo $usuario;
+/*
+--- insert login e senha ---
+$aluno = new Usuario("aluno", "@aluno");
+$aluno->insert();
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(10);
+
+$usuario->update("professor", "321654");
+
+echo $usuario;
+
+?>
